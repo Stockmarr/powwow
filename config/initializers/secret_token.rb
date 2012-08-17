2c5f1d7cf565cd3d30b674b0787273b1030b8adb
Sebastian Stockmarr <sebastian@aabenbart.dk>
2012-08-17 17:08:45 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powwow::Application.config.secret_token = '0dae8917195a869151dd231d73178ec4aa046d81540f05f2910547a5b98ef93055d3ea86cb22eb593f1f4961edc83edec52cdca400113a4674010692c6b3522c'
